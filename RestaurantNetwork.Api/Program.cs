82429042079bcc6eeec3968c9562c5546b5df425
Jegor2005 <igor.gurski0203@gmail.com>
2026-02-20 21:10:17 +0100

    add global exception handling with problem details

--- RestaurantNetwork.Api/Program.cs
+++ RestaurantNetwork.Api/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -24,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
