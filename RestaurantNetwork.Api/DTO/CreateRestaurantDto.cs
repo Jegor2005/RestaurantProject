fc950280afe391b5e9c8ddeb300d8d428d6dcc42
Jegor2005 <igor.gurski0203@gmail.com>
2026-02-20 20:01:10 +0100

    add validation for restaurant DTOs

--- RestaurantNetwork.Api/DTO/CreateRestaurantDto.cs
+++ RestaurantNetwork.Api/DTO/CreateRestaurantDto.cs
@@ -1,9 +1,17 @@
-﻿namespace RestaurantNetwork.Api.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantNetwork.Api.DTO
 {
     public class CreateRestaurantDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Color { get; set; } = "";
+        [Required]
+        [MaxLength(200)]
         public string Address { get; set; } = "";
+
+        [Range(0, 1_000_000)]
         public decimal Rent { get; set; }
     }
 }
